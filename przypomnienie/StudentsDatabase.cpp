6ad1a0b50bea34c3f05e5f716f0c446178332706
polcia96 <paulina.wiecz@gmail.com>
2018-02-27 17:50:41 +0100

    aba

--- przypomnienie/StudentsDatabase.cpp
+++ przypomnienie/StudentsDatabase.cpp
@@ -3,6 +3,7 @@
 
 
 bool StudentsDatabase::insertStudent(Student& newStudent) {
+	//iterator musi miec konkretny typ,to jest szablon !
 	std::pair<std::map<unsigned int,Student>::iterator, bool> returning;
 	returning=database.emplace(newStudent.getIndexNumber(), newStudent);
 	return returning.second;
